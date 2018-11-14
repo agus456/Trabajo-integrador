fe7260f9812deea4ca78800403decbaa1e5ee129
Agustin <agustin.pieres@hotmail.com>
2018-11-14 20:44:13 -0300

    Trabajo integrador repositorio

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="css/styles.css" rel="stylesheet">
+		  <link rel="stylesheet" href="ionicons/css/ionicons.min.css">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
+		<title>Working Group</title>
+	</head>
+	<header>
+
+			<nav class="main-nav">
+						<ul>
+
+<li><a href="registro.php"><button type="button" class="btn btn-secundary" class="registro">Registro</button></a></li>
+<li><a href="login.php"><button type="button" class="btn btn-secundary" class="login">Login</button></a></li>
+
+						</ul>
+					</nav>
+
+
+
+
+	</header>
+	<body>
+
+<div class="container">
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/dermaglos.png" alt="dermaglos">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/ofertas.png" alt="ofertas">
+				<br>
+
+					<p><strong>Dermaglos <br>Spray 170 ml<br> FPS 30</strong> </p>
+					<h4>Precio: 495</h4>
+					<h5><strong>15-11-18</strong></h5>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/coca-cola-3l.png" alt="coca cola">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/ofertas.png" alt="ofertas">
+				<br>
+
+					<p><strong>Coca Cola <br>3 Litros<br></strong> </p><br>
+					<h4>Precio: 99.90</h4>
+					<h5><strong>15-11-18</strong></h5>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/lays-classic.png" alt="lays classic">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/ofertas.png" alt="ofertas">
+				<br>
+
+					<p><strong>Papas Fritas<br>Lays<br>250 gr.</strong> </p>
+					<h4>Precio: 97</h4>
+					<h5><strong>15-11-18</strong></h5>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-image">
+					<img src="images/dermaglos.png" alt="dermaglos">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/compras.png" alt="ofertas">
+				<br>
+
+					<p><strong>Dermaglos <br>Spray 170 ml<br> FPS 30</strong> </p>
+					<h4>Precio: 455</h4>
+					<h5><strong>08-08-18</strong></h5>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/coca-cola-3l.png" alt="coca cola">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/compras.png" alt="ofertas">
+				<br>
+
+					<p><strong>Coca Cola <br>3 Litros<br></strong> </p><br>
+					<h4>Precio: 110</h4>
+					<h5><strong>15-10-18</strong></h5>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/lays-classic.png" alt="lays classic">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/compras.png" alt="ofertas">
+				<br>
+
+					<p><strong>Papas Fritas<br>Lays<br>250 gr.</strong> </p>
+					<h4>Precio: 103.20</h4>
+					<h5><strong>13-11-18</strong></h5>
+				</div>
+			</div>
+
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/dermaglos.png" alt="dermaglos">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/consultas.png" alt="ofertas">
+				<br>
+
+					<p><strong>Dermaglos <br>Spray 170 ml<br> FPS 30</strong> </p>
+					<h4>Precio: 410</h4>
+					<h5><strong>07-07-18</strong></h5>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/coca-cola-3l.png" alt="coca cola">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/consultas.png" alt="ofertas">
+				<br>
+
+					<p><strong>Coca Cola <br>3 Litros<br></strong> </p><br>
+					<h4>Precio: 85</h4>
+					<h5><strong>27-09-18</strong></h5>
+				</div>
+			</div>
+
+			<div class="card">
+				<div class="card-image">
+					<img src="images/lays-classic.png" alt="lays classic">
+
+				</div>
+				<div class="card-data">
+				<img class="special" src="images/consultas.png" alt="ofertas">
+				<br>
+
+					<p><strong>Papas Fritas<br>Lays<br>250 gr.</strong> </p>
+					<h4>Precio: 95</h4>
+					<h5><strong>11-11-18</strong></h5>
+				</div>
+			</div>
+
+</div>
+</body>
+</html>
