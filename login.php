2cd809ee5dcabd67c2630572bd2288b5d6e98bf7
lucky snchz <xul27@hotmail.com>
2018-12-04 20:12:42 -0300

    actualización Login

--- login.php
+++ login.php
@@ -9,20 +9,20 @@
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+</head>
 	<body>
-	<h1 class="mensaje">Ingresá a nuestra página!</h1>
-		<title>Working Group</title>
-	</head>
-	<div class="contacto">
+	<h1 style="margin-left:43%">Logueate!</h1>
+
+
+	<div class="contacto" style="margin-top:-2.5%">
 	<form id="registro"action="index.html" method="post">
 	<label for="email">Email email</label><br>
 	<input type="email" name="email" id="email" placeholder="Email"><br><br>
 	<label for="contraseña">Contraseña</label><br>
 	<input type="password" name="contraseña" id="contraseña" placeholder="password"><br><br>
-	<input type="checkbox" name="recordar" id="recordar" value="">Recordame<br>
-	<p>La combinación ingresada de email y contraseña no es <br>válida</p>
-	<button id="enviar-login" class="btn btn-secondary col-sm" type="button"> Enviar </button><br><br>
+	<input type="checkbox" name="recordar" id="recordar" value="" style="margin-left:-45%;margin-right:-45%">Recordame<br>
+	<p>Ingresa Usuario y Contraseña Registrados</p>
+	<button id="enviar-login" class="btn btn-secondary col-sm" type="button" style="width:100%;margin-left:-2%"> Enviar </button><br><br>
 	</form>
 </div>
 
